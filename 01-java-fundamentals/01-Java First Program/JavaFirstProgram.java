21755e8596992962bee3f1b8c72672a01273bd30
AVWestover <thenamesrey@gmail.com>
2021-11-29 12:43:58 -0600

    First Commit

---
+++ 01-java-fundamentals/01-Java First Program/JavaFirstProgram.java
@@ -0,0 +1,7 @@
+public class JavaFirstProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Audrey(Rey) Westover");
+        System.out.println("I am 20 years old");
+        System.out.println("My hometown is Springfield, IL");
+    }
+}

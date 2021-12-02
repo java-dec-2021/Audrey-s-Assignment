04416ff036e471995443af42b63a5cc1ea2a74ab
AVWestover <thenamesrey@gmail.com>
2021-12-02 13:33:32 -0600

    Later Commit

---
+++ 02-java-oop/03-zoo-keeper-pt1/GorillaTest.java
@@ -0,0 +1,16 @@
+public class GorillaTest{
+    public static void main( String[] args ) {
+    //make gorilla
+    Gorilla george = new Gorilla();
+    //throw three things
+    george.throwSomething();
+    george.throwSomething();
+    george.throwSomething();
+    //eat bananas twice
+    george.eatBananas();
+    george.eatBananas();
+    //climb once
+    george.climb();
+    //
+    }
+}
---
+++ 02-java-oop/03-zoo-keeper-pt1/Mammal.java
@@ -0,0 +1,10 @@
+public class Mammal{
+    //energy lvl
+    public int energyLevel = 100;
+    //display energy
+    public int displayEnergy(){
+        System.out.println( this.energyLevel );
+        return this.energyLevel;
+    }
+    //
+}
